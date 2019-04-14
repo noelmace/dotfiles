1bebe53def5224e6b14594e25f60783ff8cad7b2
Noel Mace <contact@noelmace.com>
2019-04-14 16:02:58 +0200

    fix: remove unstable web-greeter

--- install.sh
+++ install.sh
@@ -1,26 +1,43 @@
 # Just some notes for myself - not testing directly
 
-sudo pacman -Sy code  \
-    awesome xbacklight xorg-xmodmap xclip \
-    wpa_supplicant dialog iw elink \
-    hub openssh vim sudo stow zsh autojump \
-    powerline-fonts otf-fira-code noto-fonts-emoji \
-    libicns graphicsmagick xz yarn \
-    alsa-utils playerctl
+## Packages
 
-# system config
+sudo pacman -Sy
+
+# X Window System
+sudo pacman -S awesome xbacklight xorg-xmodmap
+# CLI utils
+sudo pacman -S xclip hub openssh vim sudo stow zsh autojump
+# WiFi
+sudo pacman -S wpa_supplicant dialog iw elink
+# Fonts
+sudo pacman -S powerline-fonts otf-fira-code noto-fonts-emoji
+# HyperJS
+sudo pacman -S libicns graphicsmagick xz yarn
+# Sound
+sudo pacman -S alsa-utils playerctl
+# LightDM 
+sudo pacman -S lightdm lightdm-gtk-greeter
+# VS Code
+sudo pacman -S code
+
+## system config
+
 sudo ln -sf $PWD/etc/fonts/conf.avail/75-noto-color-emoji.conf /etc/fonts/conf.d/
 sudo ln -sf $PWD/etc/X11/xorg.conf.d/00-keyboard.conf /etc/X11/xorg.conf.d/
 sudo ln -sf $PWD/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d/
 sudo ln -sf $PWD/etc/locale.conf /etc/
 
-# link user dotfiles
+## link user dotfiles
 # this project should be clones in $HOME/dotfiles
+
 stow awesome
 stow git
 stow hyper.js
 stow vsconf
 stow zsh
+
+## Tools
 
 # unmute master with 'm' key
 alsamixer
