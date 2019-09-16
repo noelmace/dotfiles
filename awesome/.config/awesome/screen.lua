c7414512c72c7cf6a3218e520eeda0d04c8946e0
Noel Mace <contact@noelmace.com>
2019-09-16 19:58:43 +0200

    fix: wallpaper

--- awesome/.config/awesome/screen.lua
+++ awesome/.config/awesome/screen.lua
@@ -12,7 +12,7 @@ screen.connect_signal(
       if type(wallpaper) == "function" then
         wallpaper = wallpaper(s)
       end
-      gears.wallpaper.maximized(wallpaper, s, true)
+      gears.wallpaper.tiled(wallpaper, s, true)
     end
   end
 )
