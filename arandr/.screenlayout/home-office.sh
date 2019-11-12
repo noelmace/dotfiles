9dad9e6f5c6ed722ddc6d7b6703d7d0cff8aaa0e
Noel Mace <contact@noelmace.com>
2019-11-12 11:36:13 +0100

    home-office screen layout for msi laptop

--- arandr/.screenlayout/home-office.sh
+++ arandr/.screenlayout/home-office.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off
+xrandr --output LVDS1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 1922x0 --rotate normal --output VGA1 --off --output VIRTUAL1 --off
